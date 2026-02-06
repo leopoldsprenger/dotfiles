c663ee03647b8cc3ff749c04cfca61dd2c812825
Leopold Sprenger <186564656+leopoldsprenger@users.noreply.github.com>
2026-02-06 11:36:52 +0100

    feat(nvim): change nvim distro to nvchad

---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  -- Plugin to display custom ascii art in NvDash 
+  {
+    "MaximilianLloyd/ascii.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+    },
+  },
+
+  -- custom automatic install of mason packages
+  require("plugins.mason"),
+
+  -- test new blink
+  -- { import = "nvchad.blink.lazyspec" },
+
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}
--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -3,15 +3,32 @@ return {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     opts = {
       ensure_installed = {
+        -- Python
         "basedpyright",
         "black",
         "isort",
         "ruff",
+
+        -- C / C++
         "clangd",
         "clang-format",
+
+        -- Lua
         "lua-language-server",
         "stylua",
+
+        -- Shell
         "shfmt",
+
+        -- Go
+        "gopls",
+        "golangci-lint",
+        "staticcheck",
+        "gofumpt",
+        "goimports",
+        "delve",
+
+        -- Treesitter tooling
         "tree-sitter-cli",
       },
       run_on_start = true,
@@ -19,3 +36,4 @@ return {
     },
   },
 }
+
