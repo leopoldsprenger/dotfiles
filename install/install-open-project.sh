027c79e5a3dd29a6292fa236c274bffc9a05155e
Leopold Sprenger <186564656+leopoldsprenger@users.noreply.github.com>
2026-02-21 20:44:49 +0100

    fix(open-project): change download method of install script to gh cli

--- install/install-open-project.sh
+++ install/install-open-project.sh
@@ -3,10 +3,14 @@ set -euo pipefail
 
 mkdir -p "$HOME/bin"
 
-curl -fsSL \
-  -o "$HOME/bin/open-project.sh" \
-  https://raw.githubusercontent.com/leopoldsprenger/termtools/main/open-project.sh
+TAG="v1.0.0"
+REPO="leopoldsprenger/termtools"
+ASSET="open-project.sh"
+DIR="$HOME/bin"
 
-chmod +x "$HOME/bin/open-project.sh"
+if [ ! -f "$HOME/bin/$ASSET" ]; then
+  gh release download "$TAG" --repo "$REPO" --pattern "$ASSET" --dir "$DIR"
+  chmod +x "$DIR"
+fi
 
-echo "Installed open-project.sh to ~/bin"
+echo "Installed $ASSET to $DIR"
