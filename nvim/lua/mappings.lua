02bac29466793be5a5d54e4bb2c56317ba93f7a8
Leopold Sprenger <186564656+leopoldsprenger@users.noreply.github.com>
2026-03-08 13:46:11 +0100

    feat(nvim): add support for typst as an alternative to latex

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -8,4 +8,7 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("t", "<Esc>", [[<C-\><C-n>]], { desc = "Terminal escape to normal mode" })
 
+-- Typst preview
+map("n", "<leader>pv", "<cmd>TypstPreview<CR>", { desc = "Typst preview" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -26,19 +26,92 @@ return {
     lazy = false,
   },
 
-  -- custom automatic install of mason packages
-  require("plugins.mason"),
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    opts = {
+      ensure_installed = {
+        -- Python
+        "basedpyright",
+        "black",
+        "isort",
+        "ruff",
+
+        -- C / C++
+        "clangd",
+        "clang-format",
+
+        -- Lua
+        "lua-language-server",
+        "stylua",
+
+        -- Shell
+        "shfmt",
+
+        -- Go
+        "gopls",
+        "golangci-lint",
+        "staticcheck",
+        "gofumpt",
+        "goimports",
+        "delve",
+
+        -- Treesitter tooling
+        "tree-sitter-cli",
+
+        -- Typst
+        "tinymist",
+      },
+      automatic_installation = true,
+    },
+  },
+
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    dependencies = { "williamboman/mason.nvim" },
+    opts = function()
+      local treesitter_langs = { "vim", "lua", "vimdoc", "html", "css", "javascript", "go", "python", "cpp", "typst" }
+      local lang_to_lsp = {
+        lua = "lua_ls",
+        html = "html",
+        css = "cssls",
+        javascript = "ts_ls",
+        go = "gopls",
+        python = "basedpyright",
+        cpp = "clangd",
+        typst = "tinymist",
+      }
+      local ensure_installed = {}
+      for _, lang in ipairs(treesitter_langs) do
+        if lang_to_lsp[lang] then
+          table.insert(ensure_installed, lang_to_lsp[lang])
+        end
+      end
+      return { ensure_installed = ensure_installed }
+    end,
+  },
 
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+       "html", "css", "javascript",
+       "go", "python", "cpp",
+       "typst"
+  		},
+      highlight = { enable = true },
+  	},
+  },
+
+  {
+    "chomosuke/typst-preview.nvim",
+    ft = "typst",
+    version = "1.*",
+    opts = {},
+  },
 }
