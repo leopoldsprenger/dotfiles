7c3472bb9be1edeb9cb21f57f7f3f547609ef9e8
Leopold Sprenger <186564656+leopoldsprenger@users.noreply.github.com>
2026-03-21 10:25:39 +0100

    config(nvim): change nvim to tokyonight moon theme

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -1,19 +1,33 @@
 
 local M = {}
 
+local bg_main = "#222436"
+local bg_darker = "#1e2030"
+
 M.base46 = {
-  theme = "catppuccin",
+  theme = "tokyonight",
+  hl_override = {
+    Normal = { bg = bg_main },
+    NvimTreeNormal = { bg = bg_darker },
+    NvimTreeNormalNC = { bg = bg_darker },
+    TelescopeNormal = { bg = bg_darker },
+    TelescopeBorder = { bg = bg_darker, fg = bg_darker },
+    TelescopePromptNormal = { bg = bg_darker },
+    TelescopePromptBorder = { bg = bg_darker, fg = bg_darker },
+    NormalFloat = { bg = bg_darker },
+    FloatBorder = { bg = bg_darker, fg = bg_darker },
+    NvdashBg = { bg = bg_main },
+  },
 }
 
 M.nvdash = {
   load_on_startup = true,
   header = (function()
     local ascii = require("ascii")
-    return ascii.art.misc.hydra.hydra
+    return ascii.art.text.neovim.ansi_shadow
   end)(),
 }
 
--- Automatically cd into the directory if nvim is started with a path
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = function()
     local args = vim.fn.argv()
