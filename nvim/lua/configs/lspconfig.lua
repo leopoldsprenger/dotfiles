9c4acd3d8a08ba4680d29436d70288f24da0d4c3
Leopold Sprenger <186564656+leopoldsprenger@users.noreply.github.com>
2026-03-22 15:52:35 +0100

    config(nvim): make basedpyright type checker less aggressive

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -2,5 +2,3 @@ require("nvchad.configs.lspconfig").defaults()
 
 local servers = { "lua_ls", "html", "cssls", "ts_ls", "gopls", "basedpyright", "clangd", "tinymist" }
 vim.lsp.enable(servers)
-
--- read :h vim.lsp.config for changing options of lsp servers 
